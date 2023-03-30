80f2402e61a822a07b94e74cc10e7d51f2e0ada1
dantzK <120504755+dantezzK@users.noreply.github.com>
2023-03-30 08:35:18 -0300

    responsive mobile footer

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -9,5 +9,10 @@ function menuShow() {
     }
 }
 
+// scroll navbar 
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 0)
+})
 
 
